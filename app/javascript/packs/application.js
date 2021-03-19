bcc52fffe911a8bbca47806aeff2aeb04f94e02a
chiennt1024 <chiennt1024>
2021-03-19 10:52:27 +0700

    Static-pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import jquery from 'jquery';
+window.$ = window.jquery = jquery;
+require("jquery")
+import "bootstrap"
+require('popper')
 
 Rails.start()
 Turbolinks.start()
