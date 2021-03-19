c21e540b559ec0b3b58bdf6203d5e906f7ded053
dat-kaihatsusha <dat-kaihatsusha@gmail.com>
2021-03-19 11:16:22 +0700

    login 02

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   resource :courses
   get 'courses/index'
   get 'courses/show'
+  devise_for :users,
+  path: '',
+              path_names: {sign_in: 'login' ,sign_out: 'logout' ,edit: 'profile',sign_up: 'resgistration'},
+              controllers: {omniauth_callbacks: 'omniauth_callbacks' }
+  # controllers:{omniauth_callbacks: "users/omniauth_callbacks"}
   root 'static_pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+  end
 end
