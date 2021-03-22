26b623c0ffb84ff43877ed16ec2c41790d5a785a
chiennt1024 <chiennt1024>
2021-03-22 10:21:54 +0700

    UI Courses action show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resource :courses
   get 'courses/index'
+  get 'courses/show'
   root 'static_pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
