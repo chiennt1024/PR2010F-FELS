a93259348d834b0a20e50dd1e00da90137b0686e
chiennt1024 <chiennt1024>
2021-03-25 15:41:04 +0700

    display question and answer

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,6 @@
 class Course < ApplicationRecord
-	has_many :user_courses
-	has_many :users, through: :user_courses
 	has_many :exams
+	has_many :users, through: :exams
 	has_many :objectives
 	belongs_to :category
 	has_many :questions
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :exams
+  has_many :courses, through: :exams
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
